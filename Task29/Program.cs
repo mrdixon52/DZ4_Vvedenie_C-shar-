bf5595bbb8d255b15c8a9ae536b13406b2432563
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-04 16:41:36 +0300

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+array[i] = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine($"Result: [{string.Join(", ", array)}]");
