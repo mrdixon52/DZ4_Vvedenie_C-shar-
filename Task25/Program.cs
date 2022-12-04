2ecced08a891a045c16e620e262406a6d0713ef2
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-04 16:17:09 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Enter first number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a;
+int i = 1; 
+while (i < b)
+{
+    c = c * a;
+    i++;
+}
+Console.WriteLine($"A to the power of B = {c}");
