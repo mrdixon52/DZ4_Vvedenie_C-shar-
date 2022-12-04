d2700b70ee7959044a5dc4da48011dc6904ae211
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-04 16:33:32 +0300

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0; 
+int i = 1;
+while (i < n)
+{
+    sum = sum + ((n / i) % 10);
+    i = i * 10;
+}
+Console.WriteLine($"The sum of the digits in a number: {sum}");
